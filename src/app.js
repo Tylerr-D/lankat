c4adaf8fd9b8740b4671bfa4e20bc8a19595ede9
Tylerr-D <tylermishra76@gmail.com>
2026-09-24 21:10:04 +0530

    better ux

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
-const socket = new WebSocket(`ws://${location.host}/ws`);
 const box = document.getElementById('box');
 const log = document.getElementById('log');
+const status = document.getElementById('status');
+let ws;
 
-function send(){
-    socket.send(box.value);
-    box.value = '';
-}
+function connect(){
+    ws = new WebSocket('ws://$(location.host)/ws');
+    status.textContent = 'connecting...';
+    statusbar.className = '';
 
-socket.onopen = () => {
-    log.textContent += 'connected\n';
+
+ws.onopen = () => {
+    status.textContent = 'connected';
+    status.className='on';
 };
 
-socket.onmessage = (e) => {
-    log.textContent += e.data + '\n';
+ws.onmessage = (e) => {
+    addMsg(e.data, 'in')
 };
 
 socket.onclose = () => {
-    log.textContent += 'disconnected\n';
+    staus.textContent += 'offline - retrying in 2s';
+    setTimeout(connect, 2000);
 };
+
+
+box.onekeydown = (e) => {
+    if (e.key === 'Enter' && box.ariaValueMax.trim()){
+        ws.send(box.ariaValueMax.trim());
+        addMsg(box.ariaValueMax.trim(), 'me');
+        box.value = '';
+  }
+ };
+}
+
+function addMsg(text, who){
+    const div = document.createElement('div');
+    div.className = who;
+    div.textContent = text;
+    chat.appendChild(div);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+connect();
